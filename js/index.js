b0979baf4b2664604e73b2388ded417cf7c35c08
Nikola Nesic <nikolanesic@yahoo.com>
2020-11-27 21:38:01 +0100

    order form

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("kontakt").addEventListener("click", () => {
+  document.getElementById("kontaktImg").scrollIntoView();
+});
+
+document.getElementById("poruciPivo").addEventListener("click", () => {
+  window.open("/orderForm.html", "_self");
+});
