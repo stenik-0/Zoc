d61157d79d72ffb05643cbabfb29c271f6370ef9
Nikola Nesic <nikolanesic@yahoo.com>
2020-12-03 19:30:16 +0100

    responsive

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-document.getElementById("kontakt").addEventListener("click", () => {
-  document.getElementById("kontaktImg").scrollIntoView();
+/*---- Waypoints ---- */
+const waypoint = new Waypoint({
+  element: document.querySelector(".section-about"),
+  handler: function (direction) {
+    const nav = document.querySelector("nav");
+    if (direction === "down") {
+      nav.classList.add("sticky");
+    } else {
+      nav.classList.remove("sticky");
+    }
+  },
+  offset: "60px",
 });
 
-document.getElementById("poruciPivo").addEventListener("click", () => {
-  window.open("/orderForm.html", "_self");
+/* ----- MOBILE MENU ----- */
+
+$(document).ready(function () {
+  $(".mobile-nav-icon").click(function () {
+    const nav = $(".main-nav");
+    let icon = $(".mobile-nav-icon i");
+    nav.slideToggle(200);
+
+    if (icon.hasClass("ion-navicon-round")) {
+      icon.addClass("ion-close-round");
+      icon.removeClass("ion-navicon-round");
+    } else {
+      icon.addClass("ion-navicon-round");
+      icon.removeClass("ion-close-round");
+    }
+  });
 });
